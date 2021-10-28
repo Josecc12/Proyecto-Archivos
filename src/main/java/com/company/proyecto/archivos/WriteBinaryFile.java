8a60a31b12cf1e900b68345327a8afdef1f400e1
Jose Daniel Chocoy <danielchocoycordon@hotmail.es>
2021-10-27 18:43:14 -0600

    Init branch

---
+++ src/main/java/com/company/proyecto/archivos/WriteBinaryFile.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.company.proyecto.archivos;
+
+import java.io.FileNotFoundException;
+import java.io.RandomAccessFile;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author josed
+ */
+public class WriteBinaryFile {
+    
+    private PdfMetadata pdf;
+
+    public WriteBinaryFile(PdfMetadata pdf) {
+        this.pdf = pdf;
+    }
+    
+    public void writeFile(){
+        try {
+            RandomAccessFile file=new RandomAccessFile("datos.bin","rw");
+            
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(WriteBinaryFile.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    
+    
+    
+}
